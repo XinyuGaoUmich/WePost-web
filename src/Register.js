5c9f030c8c360ee7140041a6cf24044c256a6ae4
XinyuGaoUmich <xinyugaoumich@gmail.com>
2018-05-19 10:54:53 -0400

    send request

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Form, Input, Button } from 'antd';
-
+import $ from 'jquery';
+import { Form, Input, Button, message } from 'antd';
+import { API_ROOT} from "./constants";
 
 const FormItem = Form.Item;
 
@@ -15,6 +16,20 @@ class RegistrationForm extends React.Component {
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                $.ajax({
+                    url: `${API_ROOT}/signup`,
+                    method: 'POST',
+                    data: JSON.stringify({
+                            username: values.username,
+                            password: values.password,
+                        }),
+                }).then((response) => {
+                   message.success(response);
+                }, (error) => {
+                    message.error(error.responseText);
+                }).catch((e) => {
+                    console.log(e);
+                });
             }
         });
     }
