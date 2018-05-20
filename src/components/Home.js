12d7d7d6a9d5b2d9af329bbe25e7096d29040620
XinyuGaoUmich <xinyugaoumich@gmail.com>
2018-05-20 17:58:42 -0400

    tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { GEO_OPTIONS } from "../constants"
+import { GEO_OPTIONS } from "../constants";
+import { Tabs, Button } from 'antd';
+
+
+const TabPane = Tabs.TabPane;
+
 
 export class Home extends React.Component {
     componentDidMount() {
@@ -25,9 +30,14 @@ export class Home extends React.Component {
     }
 
     render() {
+        const operations = <Button type="primary">Create New Post</Button>;
+
         return (
-            <div>
-                This is Home!
+            <div className="main-tabs">
+                <Tabs tabBarExtraContent={operations}>
+                    <TabPane tab="Posts" key="1">Posts</TabPane>
+                    <TabPane tab="Map" key="2">Map</TabPane>
+                </Tabs>
             </div>
         );
     }
