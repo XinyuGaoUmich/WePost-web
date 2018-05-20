33224975294f7882ac7a4b87ddd8e9ef94962206
XinyuGaoUmich <xinyugaoumich@gmail.com>
2018-05-20 17:28:49 -0400

    add geolocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import { GEO_OPTIONS } from "../constants"
 
 export class Home extends React.Component {
+    componentDidMount() {
+        this.getGeoLocation();
+    }
+
+    getGeoLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation, this.onFailedLoadGeoLocation, GEO_OPTIONS
+            );
+        } else {
+
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+    }
+
+    onFailedLoadGeoLocation = (error) => {
+        console.log(error);
+    }
+
     render() {
         return (
             <div>
