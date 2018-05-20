f2e57d28b25cffc8b808d3395f7d33085592406e
XinyuGaoUmich <xinyugaoumich@gmail.com>
2018-05-19 23:57:44 -0400

    localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Header } from './Header';
 import '../styles/App.css';
 import { Main } from './Main';
+import { TOKEN_KEY} from "../constants";
 
 class App extends React.Component {
+     state = {
+         isLoggedIn: Boolean(localStorage.getItem(TOKEN_KEY)),
+     }
+
+     handleLogin = (token) => {
+         localStorage.setItem(TOKEN_KEY, token);
+         this.setState({ isLoggedIn: true});
+     }
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Main/>
+        <Header isLoggedIn={this.state.isLoggedIn}/>
+        <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin}/>
       </div>
     );
   }
