f20e502c1e50b4d77d3b67c17779f15057688ba9
XinyuGaoUmich <xinyugaoumich@gmail.com>
2018-05-28 19:46:10 -0400

    final project

--- src/components/CreatePostButton.js
+++ src/components/CreatePostButton.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Modal, Button } from 'antd';
-import {WrappedCreatePostForm} from "./CreatePostForm"
+import { Modal, Button , message} from 'antd';
+import {WrappedCreatePostForm} from "./CreatePostForm";
+import $ from 'jquery';
+import {API_ROOT, AUTH_PREFIX, POS_KEY, TOKEN_KEY, LOC_SHAKE} from "../constants"
+
+
 
 export class CreatePostButton extends React.Component {
     state = {
@@ -14,16 +18,53 @@ export class CreatePostButton extends React.Component {
         });
     }
     handleOk = () => {
-        this.setState({
-            ModalText: 'The modal will be closed after two seconds',
-            confirmLoading: true,
+        this.setState({ confirmLoading: true,});
+        this.form.validateFields((err, values) => {
+            const {lat, lon} = JSON.parse(localStorage.getItem(POS_KEY));
+            const formData = new FormData();
+            formData.set('message', values.message);
+            formData.set('lat', lat + Math.random() * LOC_SHAKE * 2 - LOC_SHAKE);
+            formData.set('lon', lon + Math.random() * LOC_SHAKE * 2 - LOC_SHAKE);
+            formData.set('image', values.image[0].originFileObj);
+
+            if (!err) {
+                $.ajax({
+                    url: `${API_ROOT}/post`,
+                    method: 'POST',
+                    data: formData,
+                    headers: {
+                        Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`
+                    },
+                    processData: false,
+                    contentType: false,
+                    dataType: 'text',
+                }).then((response) => {
+                        message.success('Created a post successfully');
+                        this.form.resetFields();
+                        this.setState({
+                            visible: false,
+                            confirmLoading: false
+                        });
+                    this.props.loadNearByPosts();
+                    }, (response) =>{
+                        message.error(response.responseText);
+                    this.setState({
+                        visible: false,
+                        confirmLoading: false
+                    });
+                    }
+                ).catch((error)=>{
+                    console.log(error);
+                });
+            }
         });
-        setTimeout(() => {
-            this.setState({
-                visible: false,
-                confirmLoading: false,
-            });
-        }, 2000);
+
+        //setTimeout(() => {
+         //   this.setState({
+          //      visible: false,
+            //    confirmLoading: false,
+         //   });
+       // }, 2000);
     }
     handleCancel = () => {
         console.log('Clicked cancel button');
@@ -31,8 +72,13 @@ export class CreatePostButton extends React.Component {
             visible: false,
         });
     }
+
+    saveFormRef = (form) => {
+        this.form = form;
+    }
+
     render() {
-        const { visible, confirmLoading, ModalText } = this.state;
+        const { visible, confirmLoading } = this.state;
         return (
             <div>
                 <Button type="primary" onClick={this.showModal}>Create New Post</Button>
@@ -43,7 +89,7 @@ export class CreatePostButton extends React.Component {
                        onCancel={this.handleCancel}
                        okText="Create"
                 >
-                    <WrappedCreatePostForm/>
+                    <WrappedCreatePostForm ref={this.saveFormRef} />
                 </Modal>
             </div>
         );
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { POS_KEY } from "../constants";
 import { Gallery } from "./Gallery"
 import $ from 'jquery';
 import { CreatePostButton } from "./CreatePostButton";
+import { WrappedAroundMap } from "./AroundMap"
 
 const TabPane = Tabs.TabPane;
 
@@ -47,18 +48,19 @@ export class Home extends React.Component {
         console.log(error);
     }
 
-    loadNearByPosts = () => {
+    loadNearByPosts = (location, range) => {
         this.setState({ loadingPost: true, error: '' })
-        const {lat, lon} = JSON.parse(localStorage.getItem(POS_KEY));
+        const {lat, lon} = location ? location : JSON.parse(localStorage.getItem(POS_KEY));
+        const radius = range ? range : 20;
         $.ajax({
-            url:`${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20000`,
+            url:`${API_ROOT}/search?lat=${lat}&lon=${lon}&range=${radius}`,
             method: 'GET',
             headers: {
                 Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`
             }
         }).then((response) => {
             console.log(response);
-            this.setState({ posts: response, loadingPost: false, error: ''});
+            this.setState({ posts: response || [], loadingPost: false, error: ''});
         }, (response) => {
             console.log(response.responseText)
             this.setState({ loadingPost: false, error: 'Failed to load posts!' })
@@ -80,7 +82,7 @@ export class Home extends React.Component {
                     user: post.user,
                     src: post.url,
                     thumbnail: post.url,
-                    caption: post.messages,
+                    caption: post.message,
                     thumbnailWidth: 400,
                     thumbnailHeight: 300
 
@@ -93,7 +95,7 @@ export class Home extends React.Component {
     }
 
     render() {
-        const operations = <CreatePostButton />;
+        const operations = <CreatePostButton loadNearByPosts={this.loadNearByPosts}/>;
 
         return (
             <div className="main-tabs">
@@ -101,7 +103,16 @@ export class Home extends React.Component {
                     <TabPane tab="Posts" key="1">
                         {this.getGalleryPanelContent()}
                     </TabPane>
-                    <TabPane tab="Map" key="2">Map</TabPane>
+                    <TabPane tab="Map" key="2">
+                        <WrappedAroundMap
+                            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+                            loadingElement={<div style={{ height: `100%` }} />}
+                            containerElement={<div style={{ height: `500px` }} />}
+                            mapElement={<div style={{ height: `100%` }} />}
+                            posts={this.state.posts}
+                            loadNearByPosts={this.loadNearByPosts}
+                        />
+                        </TabPane>
                 </Tabs>
             </div>
         );
